e4d2489c154d6df17d16d03f982c33617f78cdcb
victor.jimenezmillan@gmail.com <victor.jimenezmillan@gmail.com>
2022-08-29 10:05:04 +0000

    File generated by Skaffolder - user: victor.jimenezmillan@gmail.com

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+// Redux
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+// Custom Actions
+import UserActions from '../redux/actions/UserActions';
+
+// START IMPORT ACTIONS
+
+// END IMPORT ACTIONS
+
+/** APIs
+
+**/
+
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Landing</h1>
+                    
+        
+            
+      </div>
+    );
+  }
+}
+
+// Store actions
+const mapDispatchToProps = function(dispatch) {
+  return { 
+    actionsUser: bindActionCreators(UserActions, dispatch)
+  };
+};
+
+// Validate types
+Landing.propTypes = {
+  actionsUser: PropTypes.object.isRequired
+};
+
+// Get props from state
+function mapStateToProps(state, ownProps) {
+  return {
+    user: state.LoginReducer.user
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Landing);
